3c7c04a419e34abd5a39d8cb147c486cc4f6daa5
Вадим <suguby@gmail.com>
2018-12-14 21:06:44 +0300

    сушка кода

--- day2/game.py
+++ day2/game.py
@@ -16,36 +16,55 @@ ENEMY_COUNT = 5
 BASE_X, BASE_Y = 0, -300
 
 
-def fire_missile(x, y):
+def create_missile(color, x, y, x2, y2):
     missile = turtle.Turtle(visible=False)
     missile.speed(0)
-    missile.color('white')
+    missile.color(color)
     missile.penup()
-    missile.setpos(x=BASE_X, y=BASE_Y)
+    missile.setpos(x=x, y=y)
     missile.pendown()
-    heading = missile.towards(x, y)
+    heading = missile.towards(x2, y2)
     missile.setheading(heading)
     missile.showturtle()
-    info = {'missile': missile, 'target': [x, y],
+    info = {'missile': missile, 'target': [x2, y2],
             'state': 'launched', 'radius': 0}
+    return info
+
+
+def fire_missile(x, y):
+    info = create_missile(color='white', x=BASE_X, y=BASE_Y, x2=x, y2=y)
     our_missiles.append(info)
 
 
 def fire_enemy_missile():
-    missile = turtle.Turtle(visible=False)
-    missile.speed(0)
-    missile.color('red')
-    missile.penup()
     x = random.randint(-600, 600)
     y = 400
-    missile.setpos(x=x, y=y)
-    missile.pendown()
-    heading = missile.towards(BASE_X, BASE_Y)
-    missile.setheading(heading)
-    missile.showturtle()
-    info = {'missile': missile, 'target': [BASE_X, BASE_Y],
-            'state': 'launched', 'radius': 0}
+    info = create_missile(color='red', x=x, y=y, x2=BASE_X, y2=BASE_Y)
     enemy_missiles.append(info)
+
+
+def move_missiles(missiles):
+    for info in missiles:
+        state = info['state']
+        missile = info['missile']
+        if state == 'launched':
+            missile.forward(4)
+            target = info['target']
+            if missile.distance(x=target[0], y=target[1]) < 20:
+                info['state'] = 'explode'
+                missile.shape('circle')
+        elif state == 'explode':
+            info['radius'] += 1
+            if info['radius'] > 5:
+                missile.clear()
+                missile.hideturtle()
+                info['state'] = 'dead'
+            else:
+                missile.shapesize(info['radius'])
+
+    dead_missiles = [info for info in missiles if info['state'] == 'dead']
+    for dead in dead_missiles:
+        missiles.remove(dead)
 
 
 window.onclick(fire_missile)
@@ -56,49 +75,8 @@ enemy_missiles = []
 while True:
     window.update()
 
-    for info in our_missiles:
-        state = info['state']
-        missile = info['missile']
-        if state == 'launched':
-            missile.forward(4)
-            target = info['target']
-            if missile.distance(x=target[0], y=target[1]) < 20:
-                info['state'] = 'explode'
-                missile.shape('circle')
-        elif state == 'explode':
-            info['radius'] += 1
-            if info['radius'] > 5:
-                missile.clear()
-                missile.hideturtle()
-                info['state'] = 'dead'
-            else:
-                missile.shapesize(info['radius'])
-
-    dead_missiles = [info for info in our_missiles if info['state'] == 'dead']
-    for dead in dead_missiles:
-        our_missiles.remove(dead)
-
     if len(enemy_missiles) < ENEMY_COUNT:
         fire_enemy_missile()
 
-    for info in enemy_missiles:
-        state = info['state']
-        missile = info['missile']
-        if state == 'launched':
-            missile.forward(4)
-            target = info['target']
-            if missile.distance(x=target[0], y=target[1]) < 20:
-                info['state'] = 'explode'
-                missile.shape('circle')
-        elif state == 'explode':
-            info['radius'] += 1
-            if info['radius'] > 5:
-                missile.clear()
-                missile.hideturtle()
-                info['state'] = 'dead'
-            else:
-                missile.shapesize(info['radius'])
-
-    dead_missiles = [info for info in enemy_missiles if info['state'] == 'dead']
-    for dead in dead_missiles:
-        enemy_missiles.remove(dead)
+    move_missiles(missiles=our_missiles)
+    move_missiles(missiles=enemy_missiles)
